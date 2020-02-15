a3b5286551a522b9d6ac5002011993c09c6a7f08
julianahrens <julianahrens@users.noreply.github.com>
2020-02-15 16:39:34 +0100

    adding terraform build for bucket

---
+++ terraform/outputs.tf
@@ -0,0 +1,25 @@
+
+output "name" {
+  description = "The name of the bucket"
+  value       = google_storage_bucket.this.name
+}
+
+output "location" {
+  description = "The GCS location of the bucket"
+  value       = google_storage_bucket.this.location
+}
+
+output "storage_class" {
+  description = "The Storage Class of the bucket"
+  value       = google_storage_bucket.this.storage_class
+}
+
+output "versioning" {
+  description = "The bucket's versioning configuration"
+  value       = google_storage_bucket.this.versioning
+}
+
+output "url" {
+  description = "The base URL of the bucket, in the format gs://<bucket-name>"
+  value       = google_storage_bucket.this.url
+}
